479d210864beb4b488603fb251ad39561f0c69cb
Moussi Aymen <moussi.aymen@hotmail.com>
2017-05-22 17:36:55 +0200

    add client

---
+++ microservice1-client/src/main/java/com/moussi/microservice1client/Microservice1ClientApplication.java
@@ -0,0 +1,12 @@
+package com.moussi.microservice1client;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Microservice1ClientApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Microservice1ClientApplication.class, args);
+	}
+}
---
+++ microservice1-client/src/test/java/com/moussi/microservice1client/Microservice1ClientApplicationTests.java
@@ -0,0 +1,16 @@
+package com.moussi.microservice1client;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class Microservice1ClientApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
