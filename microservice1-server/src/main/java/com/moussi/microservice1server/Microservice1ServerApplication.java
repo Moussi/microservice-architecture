c0bac43850d82db84ebd52ca7cc6fb1221d7cbf0
Moussi Aymen <moussi.aymen@hotmail.com>
2017-05-22 16:45:56 +0200

    add configData repository

--- microservice1-server/src/main/java/com/moussi/microservice1server/Microservice1ServerApplication.java
+++ microservice1-server/src/main/java/com/moussi/microservice1server/Microservice1ServerApplication.java
@@ -2,13 +2,19 @@ package com.moussi.microservice1server;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.support.SpringBootServletInitializer;
 import org.springframework.cloud.config.server.EnableConfigServer;
 
 @SpringBootApplication
 @EnableConfigServer
-public class Microservice1ServerApplication {
+public class Microservice1ServerApplication  extends SpringBootServletInitializer{
 
 	public static void main(String[] args) {
 		SpringApplication.run(Microservice1ServerApplication.class, args);
+	}
+
+	@Override protected SpringApplicationBuilder configure(SpringApplicationBuilder builder) {
+		return builder.sources(Microservice1ServerApplication.class);
 	}
 }
