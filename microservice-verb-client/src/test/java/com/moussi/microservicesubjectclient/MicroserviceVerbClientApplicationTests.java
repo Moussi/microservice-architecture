0aaa13d2a580d2ae069610e5a6bfc1408eb31ed9
MOUSSI Aymen <moussi.aymen@hotmail.com>
2017-05-25 21:27:05 +0200

    add microservices and setup eureka discovery

---
+++ microservice-verb-client/src/test/java/com/moussi/microservicesubjectclient/MicroserviceVerbClientApplicationTests.java
@@ -0,0 +1,16 @@
+package com.moussi.microservicesubjectclient;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class MicroserviceVerbClientApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
